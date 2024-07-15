1a00fb5257208395f08f3ecaade626475464457a
Kostiantyn Boiar <opezdalya@gmail.com>
2024-07-15 05:33:48 -0500

    Commit

--- react-app/envy-bot-frontend/src/App.js
+++ react-app/envy-bot-frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react"
+import Main from './Components/Main';
+
 
 function App() {
+
+  const tele = window.Telegram.WebApp;
+  
+  useEffect(() => {
+    tele.ready();
+  })
+
   return (
-<div>
-  Hello, world!
-</div>
+    <Main></Main>
   );
 }
 
---
+++ react-app/envy-bot-frontend/src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css"
+function Main() {
+    
+    return(
+        <div class = "container">
+            <h1> Hello!</h1>
+        </div>
+    );
+}
+
+export default Main;
