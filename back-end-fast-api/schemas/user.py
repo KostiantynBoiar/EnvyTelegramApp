c1f0a66319dc2c044eb63f1a327c1da7f0061ed4
Kostiantyn Boiar <opezdalya@gmail.com>
2024-07-22 07:37:47 -0500

    Add UserBaseSchema

---
+++ back-end-fast-api/schemas/user.py
@@ -0,0 +1,19 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class UserBaseSchema(BaseModel):
+
+    id = str
+    telegram_username = str
+    count_of_coins = str
+    last_time_of_the_claim = str
+    referal_link = str
+    reffered_by = str
+
+
+    
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
