053686b7b5598a034775e2e1980886e2787531b2
Kostiantyn Boiar <97537403+KostiantynBoiar@users.noreply.github.com>
2024-07-29 17:33:17 +0100

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -2,16 +2,19 @@ from sqlalchemy.orm import relationship
 
 from database import Base
 from sqlalchemy import TIMESTAMP, Column, String, Boolean, Integer
-
+import secrets
 
 class User(Base):
-    
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     telegram_username = Column(String, nullable=False)
-    count_of_coins = Column(Integer)
+    count_of_coins = Column(Integer, default=0)
     last_time_of_the_claim = Column(String)
-    referal_link = Column(String)
+    referal_link = Column(String, unique=True, default=lambda: secrets.token_urlsafe(8))
     tasks = relationship("Task", back_populates="user")
-    reffered_by = Column(String)
+    reffered_by = Column(Integer)
+
+    def generate_referral_link(self):
+        return secrets.token_urlsafe(8)
+
