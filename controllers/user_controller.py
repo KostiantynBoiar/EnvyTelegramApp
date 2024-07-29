882cf340b90afb4d8ac5ec08dcc1a8fd1a6c7439
Kostiantyn Boiar <97537403+KostiantynBoiar@users.noreply.github.com>
2024-07-29 17:39:39 +0100

    Update user_controller.py

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -2,7 +2,8 @@ import sys
 import os
 from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List, Optional
+from models.task_model import Task
 
 from schemas.user_schema import UserBaseSchema, UserCreateSchema
 
@@ -11,6 +12,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../models'))
 from models.user_model import User
 from database import get_db, Base, engine
 
+#Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -38,6 +40,18 @@ def create_user(user: UserCreateSchema, db: Session = Depends(get_db)):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+
+    tasks = db.query(Task).all()
+    for task in tasks:
+        new_task = Task(
+            title=task.title,
+            description=task.description,
+            award=task.award,
+            user_id=db_user.id
+        )
+        db.add(new_task)
+    db.commit()
+
     return db_user
 
 
@@ -62,4 +76,44 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
     db.commit()
     return db_user
 
+
+@router.post("/referral", response_model=UserBaseSchema)
+def create_user_with_referral(user: UserCreateSchema, referal_link: str, db: Session = Depends(get_db)):
+    referrer = db.query(User).filter(User.referal_link == referal_link).first()
+    if not referrer:
+        raise HTTPException(status_code=404, detail="Invalid referral link")
+    
+    user_data = user.dict()
+    user_data['reffered_by'] = referrer.id
+    if 'reffered_by' in user_data:
+        del user_data['reffered_by']
+    
+    db_user = User(**user_data, reffered_by=referrer.id)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    tasks = db.query(Task).all()
+    for task in tasks:
+        new_task = Task(
+            title=task.title,
+            description=task.description,
+            award=task.award,
+            user_id=db_user.id
+        )
+        db.add(new_task)
+    db.commit()
+    return db_user
+
+
+@router.get("/name/{telegram_username}", response_model=Optional[int])
+def get_user_id_by_telegram_username(telegram_username: str, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.telegram_username == telegram_username).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user.id
+
+
 app.include_router(router)
+
+
