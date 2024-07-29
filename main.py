115d76c29f7630724444d5d2258b93210bd361cf
Kostiantyn Boiar <97537403+KostiantynBoiar@users.noreply.github.com>
2024-07-29 16:07:24 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -34,7 +34,4 @@ def read_root():
     return {"Hello": "World"}
 
 
-if __name__ == '__main__':
 
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=3232, log_level="info", reload=False)
