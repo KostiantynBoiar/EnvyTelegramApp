d761337bf42a6b41d0aa551d3dbb956155834467
opezdalya <opezdalya@gmail.com>
2024-08-07 15:34:44 +0100

    update

--- python-bot/bot.py
+++ python-bot/bot.py
@@ -25,6 +25,7 @@ app = Flask(__name__)
 
 dp = Dispatcher()
 
+"""
 @dp.message(Command("bot"))
 async def bot_web_handler(message: Message):
     markup = InlineKeyboardMarkup(
@@ -38,15 +39,25 @@ async def bot_web_handler(message: Message):
         ]
     )
     await message.reply("That's your app: ", reply_markup=markup)
-
+"""
 @dp.message(CommandStart())
 async def start(message: Message):
+    markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Web App",
+                    web_app=types.WebAppInfo(url=URL)
+                )
+            ]
+        ]
+    )
     username = message.from_user.username if message.from_user.username is not None else f'{message.from_user.first_name} {message.from_user.last_name}'
     request = create_user(message.from_user.id, username)
     if request in (500, 400):
-        await message.reply(f"Hi, {username}! Thank you for visiting us again")
+        await message.reply(f"Hi, {username}! Thank you for visiting us again, that's your app: ", reply_markup=markup)
     elif request == 200:
-        await message.reply(f'Hello, {username}')
+        await message.reply(f"Hello, {username} that's your app", reply_markup=markup)
 
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
