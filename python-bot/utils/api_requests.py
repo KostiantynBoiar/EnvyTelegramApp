140b52e869c3ae238dda3916cf9a87bf07d683cc
opezdalya <opezdalya@gmail.com>
2024-08-08 18:19:47 +0100

    qfix

--- python-bot/utils/api_requests.py
+++ python-bot/utils/api_requests.py
@@ -4,7 +4,21 @@ from os import getenv
 from dotenv import load_dotenv
 from datetime import datetime
 import logging
+
+# Load environment variables from a .env file
 load_dotenv()
+
+# Set up logging configuration
+logging.basicConfig(
+    level=logging.INFO,  # Log level can be set to DEBUG, INFO, WARNING, ERROR, or CRITICAL
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout),  # Log to stdout
+        logging.FileHandler(f'create_user_{datetime.now().strftime("%Y%m%d")}.log'),  # Log to a file
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 URL = getenv("API_URL")
 
@@ -13,11 +27,17 @@ def create_user(user_id: str, username: str):
         'telegram_id': f"{user_id}",
         'telegram_username': username
     }
-    print(data)
-    response = requests.post(f'{URL}/api/v1/users/', json=data)
-    if response.status_code == 200:
-        print(response.status_code)
+    # Log the data being sent
+    logger.info(f"Creating user with data: {data}")
+    
+    try:
+        response = requests.post(f'{URL}/api/v1/users/', json=data)
+        response.raise_for_status()  # This will raise an HTTPError for bad responses
+        logger.info(f"User created successfully. Status code: {response.status_code}")
         return 200
-    else:
-        print(f"Something went wrong! Status code: {response.status_code},\n {response}")
+    except requests.exceptions.HTTPError as http_err:
+        logger.error(f"HTTP error occurred: {http_err}")
         return response.status_code
+    except Exception as err:
+        logger.error(f"Other error occurred: {err}")
+        return None
