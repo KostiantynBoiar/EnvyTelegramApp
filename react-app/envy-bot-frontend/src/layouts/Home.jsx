058b904c3f1c7b6839b37e21b391bfab72e9a69a
opezdalya <opezdalya@gmail.com>
2024-08-17 23:11:59 +0100

    update

--- react-app/envy-bot-frontend/src/layouts/Home.jsx
+++ react-app/envy-bot-frontend/src/layouts/Home.jsx
@@ -22,7 +22,6 @@ export const Home = () => {
     useEffect(() => {
         const fetchUserData = async () => {
 			const user_id = await getUserId(tg.initDataUnsafe.user.id);
-
             //const user_id = await getUserId("506652203");
             if (user_id) {
                 fetch(`https://envytelegramapp.onrender.com/api/v1/users/${user_id}`, {
